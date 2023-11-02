2650865c5590ea0783873c31e2c50721f989607e
Animesh Raj <animeshraj223@gmail.com>
2023-11-02 23:36:09 +0530

    adding changes

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("Starting the application")
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// database
+const db = require("./app/models");
+const Role = db.role;
+
+db.sequelize.sync();
+
+
+// simple route
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to my application." });
+});
+
+// routes
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
